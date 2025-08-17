e34c764c6e19343c5a959870f5f395c768f02a19
Naveen <naveendhan06@gmail.com>
2025-08-17 16:30:19 +0530

    Added assertions in step

--- src/test/java/stepDefinitions/Steps.java
+++ src/test/java/stepDefinitions/Steps.java
@@ -13,114 +13,116 @@ import pageObjects.LobbyPage;
 import pageObjects.LoginPage;
 import pageObjects.PracticePage;
 import utils.DriverUtils;
+import static org.junit.Assert.assertTrue;
 
 public class Steps {
 
-    private static final Logger logger = LogManager.getLogger(Steps.class);
+	private static final Logger logger = LogManager.getLogger(Steps.class);
 
-    private HomePage hp;
-    private PracticePage pp;
-    private LoginPage lp;
-    private LobbyPage lbp;
+	private HomePage hp;
+	private PracticePage pp;
+	private LoginPage lp;
+	private LobbyPage lbp;
 
-    @Given("User launches the browser")
-    public void user_launches_the_browser() {
-        DriverUtils.setCurrentMethodName(new Object(){}.getClass().getEnclosingMethod().getName());
-        DriverFactory.getDriver().manage().window().maximize();
-        logger.info("Browser launched and maximized");
-    }
 
-    @Given("navigates to the application URL")
-    public void navigates_to_the_application_url() {
-        DriverUtils.setCurrentMethodName(new Object(){}.getClass().getEnclosingMethod().getName());
-        String appUrl = ConfigLoader.getProperty("url");
-        DriverFactory.getDriver().get(appUrl);
-        logger.info("Navigated to application URL: {}", appUrl);
-    }
+	@Given("User launches the browser")
+	public void user_launches_the_browser() {
+		DriverUtils.setCurrentMethodName(new Object(){}.getClass().getEnclosingMethod().getName());
+		DriverFactory.getDriver().manage().window().maximize();
+		logger.info("Browser launched and maximized");
+	}
 
-    @When("User validates the Home Page")
-    public void user_validates_the_home_page() {
-        DriverUtils.setCurrentMethodName(new Object(){}.getClass().getEnclosingMethod().getName());
-        hp = new HomePage(DriverFactory.getDriver());
-        hp.verifyHomePage();
-        logger.info("Home Page validated successfully");
-    }
+	@Given("navigates to the application URL")
+	public void navigates_to_the_application_url() {
+		DriverUtils.setCurrentMethodName(new Object(){}.getClass().getEnclosingMethod().getName());
+		String appUrl = ConfigLoader.getProperty("url");
+		DriverFactory.getDriver().get(appUrl);
+		logger.info("Navigated to application URL: {}", appUrl);
+	}
 
-    @When("clicks on Practice")
-    public void clicks_on_practice() {
-        DriverUtils.setCurrentMethodName(new Object(){}.getClass().getEnclosingMethod().getName());
-        hp.clickPractice();
-        logger.info("Clicked on Practice link");
-    }
+	@When("User validates the Home Page")
+	public void user_validates_the_home_page() {
+		DriverUtils.setCurrentMethodName(new Object(){}.getClass().getEnclosingMethod().getName());
+		hp = new HomePage(DriverFactory.getDriver());
+		assertTrue("Home Page not displayed!", hp.verifyHomePage());
+		logger.info("Home Page validated successfully");
+	}
 
-    @Then("User lands on the Practice Page")
-    public void user_lands_on_the_practice_page() {
-        DriverUtils.setCurrentMethodName(new Object(){}.getClass().getEnclosingMethod().getName());
-        pp = new PracticePage(DriverFactory.getDriver());
-        pp.verifyPracticePage();
-        logger.info("Practice Page verified successfully");
-    }
+	@When("clicks on Practice")
+	public void clicks_on_practice() {
+		DriverUtils.setCurrentMethodName(new Object(){}.getClass().getEnclosingMethod().getName());
+		hp.clickPractice();
+		logger.info("Clicked on Practice link");
+	}
 
-    @When("User clicks on Test Login")
-    public void user_clicks_on_test_login() {
-        DriverUtils.setCurrentMethodName(new Object(){}.getClass().getEnclosingMethod().getName());
-        pp.clickLogin();
-        logger.info("Clicked on Test Login link");
-    }
+	@Then("User lands on the Practice Page")
+	public void user_lands_on_the_practice_page() {
+		DriverUtils.setCurrentMethodName(new Object(){}.getClass().getEnclosingMethod().getName());
+		pp = new PracticePage(DriverFactory.getDriver());
+		assertTrue("Practice Page not displayed!", pp.verifyPracticePage());
+		logger.info("Practice Page verified successfully");
+	}
 
-    @Then("User lands on the Test Page")
-    public void user_lands_on_the_test_page() {
-        DriverUtils.setCurrentMethodName(new Object(){}.getClass().getEnclosingMethod().getName());
-        lp = new LoginPage(DriverFactory.getDriver());
-        lp.isLoginPage();
-        logger.info("Test Page validated successfully");
-    }
+	@When("User clicks on Test Login")
+	public void user_clicks_on_test_login() {
+		DriverUtils.setCurrentMethodName(new Object(){}.getClass().getEnclosingMethod().getName());
+		pp.clickLogin();
+		logger.info("Clicked on Test Login link");
+	}
 
-    @When("User navigates to Test URL")
-    public void user_navigates_to_test_url() {
-        DriverUtils.setCurrentMethodName(new Object(){}.getClass().getEnclosingMethod().getName());
-        String testUrl = ConfigLoader.getProperty("test_url");
-        DriverFactory.getDriver().get(testUrl);
-        lp = new LoginPage(DriverFactory.getDriver());
-        lp.isLoginPage();
-        logger.info("Navigated to Test URL: {}", testUrl);
-    }
+	@Then("User lands on the Test Page")
+	public void user_lands_on_the_test_page() {
+		DriverUtils.setCurrentMethodName(new Object(){}.getClass().getEnclosingMethod().getName());
+		lp = new LoginPage(DriverFactory.getDriver());
+		assertTrue("Login Page not displayed!", lp.isLoginPage());
+		logger.info("Test Page validated successfully");
+	}
 
-    @When("User enters valid credentials")
-    public void user_enters_valid_credentials() {
-        DriverUtils.setCurrentMethodName(new Object(){}.getClass().getEnclosingMethod().getName());
-        String username = ConfigLoader.getProperty("username");
-        String password = ConfigLoader.getProperty("password");
-        lp.enterCreds(username, password);
-        logger.info("Entered credentials: username={} password=******", username);
-    }
+	@When("User navigates to Test URL")
+	public void user_navigates_to_test_url() {
+		DriverUtils.setCurrentMethodName(new Object(){}.getClass().getEnclosingMethod().getName());
+		String testUrl = ConfigLoader.getProperty("test_url");
+		DriverFactory.getDriver().get(testUrl);
+		lp = new LoginPage(DriverFactory.getDriver());
+		assertTrue("Login Page not displayed after navigating!", lp.isLoginPage());
+		logger.info("Navigated to Test URL: {}", testUrl);
+	}
 
-    @When("clicks on Submit")
-    public void clicks_on_submit() {
-        DriverUtils.setCurrentMethodName(new Object(){}.getClass().getEnclosingMethod().getName());
-        lp.clickSubmit();
-        logger.info("Clicked Submit button");
-    }
+	@When("User enters valid credentials")
+	public void user_enters_valid_credentials() {
+		DriverUtils.setCurrentMethodName(new Object(){}.getClass().getEnclosingMethod().getName());
+		String username = ConfigLoader.getProperty("username");
+		String password = ConfigLoader.getProperty("password");
+		lp.enterCreds(username, password);
+		logger.info("Entered credentials: username={} password=******", username);
+	}
 
-    @Then("User lands on the Login Success page")
-    public void user_lands_on_the_login_success_page() {
-        DriverUtils.setCurrentMethodName(new Object(){}.getClass().getEnclosingMethod().getName());
-        lbp = new LobbyPage(DriverFactory.getDriver());
-        lbp.isLoginSuccess();
-        logger.info("Login Success page verified");
-    }
+	@When("clicks on Submit")
+	public void clicks_on_submit() {
+		DriverUtils.setCurrentMethodName(new Object(){}.getClass().getEnclosingMethod().getName());
+		lp.clickSubmit();
+		logger.info("Clicked Submit button");
+	}
 
-    @When("User clicks on Logout")
-    public void user_clicks_on_logout() {
-        DriverUtils.setCurrentMethodName(new Object(){}.getClass().getEnclosingMethod().getName());
-        lbp.clickLogOut();
-        logger.info("Clicked Logout button");
-    }
+	@Then("User lands on the Login Success page")
+	public void user_lands_on_the_login_success_page() {
+		DriverUtils.setCurrentMethodName(new Object(){}.getClass().getEnclosingMethod().getName());
+		lbp = new LobbyPage(DriverFactory.getDriver());
+		assertTrue("Login Success page not displayed!", lbp.isLoginSuccess());
+		logger.info("Login Success page verified");
+	}
 
-    @Then("User is logged out successfully")
-    public void user_is_logged_out_successfully() {
-        DriverUtils.setCurrentMethodName(new Object(){}.getClass().getEnclosingMethod().getName());
-        lp.isLoginPage();
-        logger.info("Verified user is logged out successfully");
-    }
+	@When("User clicks on Logout")
+	public void user_clicks_on_logout() {
+		DriverUtils.setCurrentMethodName(new Object(){}.getClass().getEnclosingMethod().getName());
+		lbp.clickLogOut();
+		logger.info("Clicked Logout button");
+	}
+
+	@Then("User is logged out successfully")
+	public void user_is_logged_out_successfully() {
+		DriverUtils.setCurrentMethodName(new Object(){}.getClass().getEnclosingMethod().getName());
+		assertTrue("User is not logged out properly!", lp.isLoginPage());
+		logger.info("Verified user is logged out successfully");
+	}
 }
